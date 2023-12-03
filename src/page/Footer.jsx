1af5b16381ec278e99495f98f8c25508e6b4c5ff
pang sunju <92684379+pang-i@users.noreply.github.com>
2023-12-03 20:32:30 +0900

    footer 및 main 구현

--- src/page/Footer.jsx
+++ src/page/Footer.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import styled from "styled-components";
+import AppStore from "../assets/AppStore.svg";
 
 const Footer = () => {
-  return <div>푸터지롱</div>;
+  return (
+    <FooterWrap>
+      <FooterMenu>
+        <FooterMenuList>
+          <MenuUl>
+            <MenuLi>중고거래</MenuLi>
+            <MenuLi>동네업체</MenuLi>
+            <MenuLi>당근알바</MenuLi>
+            <MenuLi>부동산 직거래</MenuLi>
+            <MenuLi>중고차 직거래</MenuLi>
+          </MenuUl>
+          <MenuUl>
+            <MenuLi>당근비즈니스</MenuLi>
+            <MenuLi>채팅하기</MenuLi>
+          </MenuUl>
+          <MenuUl>
+            <MenuLi>자주 묻는 질문</MenuLi>
+            <MenuLi>회사 소개</MenuLi>
+            <MenuLi>인재 채용</MenuLi>
+          </MenuUl>
+        </FooterMenuList>
+        <div>
+          <p>당근 앱 다운로드</p>
+          <div>
+            <button>App Store</button>
+            <button>Google Play</button>
+          </div>
+        </div>
+      </FooterMenu>
+    </FooterWrap>
+  );
 };
 
 export default Footer;
+
+const FooterWrap = styled.div`
+  width: 100%;
+  height: 390px;
+  border: 1px solid red;
+`;
+
+const FooterMenu = styled.div`
+  display: flex;
+  padding: 4rem;
+  justify-content: center;
+
+  @media screen and (max-width: 1000px) {
+    width: 1000px;
+    justify-content: flex-start;
+  }
+`;
+
+const FooterMenuList = styled.div`
+  display: flex;
+`;
+const MenuUl = styled.ul`
+  margin-right: 2rem;
+`;
+const MenuLi = styled.li`
+  list-style-type: none;
+  margin-bottom: 1.5rem;
+`;
--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import styled from "styled-components";
+import Character from "../assets/img/mainCharacter.webp";
 
 const MainPage = () => {
-  return <div>메인이지롱</div>;
+  return (
+    <>
+      <MainTop>
+        <TitleWrap>
+          <TitleText>
+            당신 근처의
+            <br />
+            지역 생활 커뮤니티
+          </TitleText>
+          <p>동네라서 가능한 모든 것</p>
+          <p>당근에서 가까운 이웃과 함께해요.</p>
+          <div>
+            <button>Google Play</button>
+            <button>App Store</button>
+          </div>
+        </TitleWrap>
+        <ImgDiv>
+          <ImgChar src={Character} />
+        </ImgDiv>
+      </MainTop>
+    </>
+  );
 };
 
 export default MainPage;
+
+const MainTop = styled.div`
+  background-color: #fffae0;
+  height: 719px;
+  display: flex;
+`;
+
+const TitleWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50%;
+  margin-left: 4rem;
+`;
+
+const TitleText = styled.p`
+  font-size: 4rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const ImgDiv = styled.div`
+  border: 1px solid red;
+  width: 50%;
+  height: 100%;
+`;
+
+const ImgChar = styled.img`
+  width: 100%;
+  height: 100%;
+`;
